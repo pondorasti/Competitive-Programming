4c53d601c6c3fc2f58a5117b3dc36da69608bc77
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-14 20:31:52 -0700

    Add HackerRank: Birthday Cake Candles

---
+++ HackerRank/Birthday Cake Candles.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+
+def birthdayCakeCandles(candles):
+    counter = 0
+    tallestCandle = 0
+    for candle in candles:
+        if candle > tallestCandle:
+            counter = 1
+            tallestCandle = candle
+        elif candle == tallestCandle:
+            counter += 1
+
+    return counter
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
