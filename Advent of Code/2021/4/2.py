d01bee81dbd3d1a70c0d07c3307e00b320239867
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-04 13:50:23 -0800

    Add AoC 2021: Day 4

---
+++ Advent of Code/2021/4/2.py
@@ -0,0 +1,67 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+
+marks = []
+boards = []
+currentMark = 0
+marked = set()
+ans = 0
+
+for line in lines:
+    if marks == []:
+        marks = line.strip("\n").split(",")
+    elif line == '\n':
+        boards.append([])
+    else:
+        lastBoard = len(boards) - 1
+        boards[lastBoard].append(line.strip("\n").split())
+
+
+def sumBoard(board):
+    sum = 0
+    for line in board:
+        for number in line:
+            if number not in marked:
+                sum += int(number)
+
+    return sum
+
+
+def checkBoard(board):
+    # rows
+    for i in range(0, len(board)):
+        streak = True
+        for j in range(0, len(board[i])):
+            if board[i][j] not in marked:
+                streak = False
+                break
+
+        if streak:
+            return sumBoard(board)
+
+    # columns
+    for j in range(0, len(board[0])):
+        streak = True
+        for i in range(0, len(board)):
+            if board[i][j] not in marked:
+                streak = False
+                break
+
+        if streak:
+            return sumBoard(board)
+
+    return None
+
+
+for mark in marks:
+    marked.add(mark)
+
+    for board in boards[:]:
+        sum = checkBoard(board)
+        if sum is not None:
+            ans = sum * int(marks[currentMark])
+            boards.remove(board)
+
+    currentMark += 1
+
+print(ans)
