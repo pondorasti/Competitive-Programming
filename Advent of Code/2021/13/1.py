b62778a0b4310647492121fc305a8de75ab5d23a
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-13 21:44:48 -0800

    Add AoC 2021: Day 13

---
+++ Advent of Code/2021/13/1.py
@@ -0,0 +1,91 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+
+# Variables
+paper = [[]]
+instructions = []
+
+paperCoordinates = []
+maxX = 0
+maxY = 0
+
+# Print Paper Helper
+
+
+def printPaper():
+    for x in range(maxX):
+        line = ""
+        for y in range(maxY):
+            line += str(paper[x][y])
+        print(line)
+
+
+# Parse Input
+readingPaper = True
+for line in lines:
+    if line == "\n":
+        readingPaper = False
+        continue
+
+    if readingPaper:
+        y, x = line.strip().split(",")
+
+        x = int(x)
+        y = int(y)
+
+        maxX = max(maxX, x)
+        maxY = max(maxY, y)
+
+        paperCoordinates.append((x, y))
+    else:
+        direction, value = line.strip().split(" ")[2].split("=")
+        instructions.append((direction, int(value)))
+
+# Create Map
+maxX += 1
+maxY += 1
+while maxX > len(paper):
+    paper.append([])
+for x in range(len(paper)):
+    while maxY > len(paper[x]):
+        paper[x].append(".")
+for coordinate in paperCoordinates:
+    x, y = coordinate
+    paper[x][y] = "#"
+
+# Fold Instructions
+direction, value = instructions[0]
+startX = 0
+startY = 0
+if direction == "y":
+    startX = value + 1
+else:
+    startY = value + 1
+for x in range(startX, len(paper)):
+    for y in range(startY, len(paper[x])):
+        if paper[x][y] == "#":
+            newX = x
+            newY = y
+
+            if direction == "y":
+                newX = (startX - 2) - (x - startX)
+            else:
+                newY = (startY - 2) - (y - startY)
+
+            paper[newX][newY] = "#"
+
+# Update size
+if direction == "y":
+    maxX = startX - 1
+else:
+    maxY = startY - 1
+
+# printPaper()
+
+# Count Marks
+ans = 0
+for x in range(maxX):
+    for y in range(maxY):
+        if paper[x][y] == "#":
+            ans += 1
+print(ans)
