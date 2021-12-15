ba69dc081e3a61a3fd2c57b8413d8c9ab8dcf056
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-14 17:28:22 -0800

    Update AoC 2021: Day 8

--- Advent of Code/2021/8/2.py
+++ Advent of Code/2021/8/2.py
@@ -3,98 +3,74 @@ with open('input.txt') as f:
 
 
 patterns = {
-    "abefg": 0, "cf": 1, "acdeg": 2, "acdfg": 3, "bcdf": 4,
+    "abcefg": 0, "cf": 1, "acdeg": 2, "acdfg": 3, "bcdf": 4,
     "abdfg": 5, "abdefg": 6, "acf": 7, "abcdefg": 8, "abcdfg": 9
 }
+permutations = []
+defaultPermutation = "abcdefg"
 
+
+def getPermutations(permutation):
+    if len(permutation) == 7:
+        keys = {}
+        for index, char in enumerate(permutation):
+            keys[defaultPermutation[index]] = char
+        permutations.append(keys)
+        return
+
+    for char in defaultPermutation:
+        if char not in permutation:
+            getPermutations(permutation + [char])
+
+
+def sortWord(word):
+    return "".join(sorted(word))
+
+
+def convert(word, keys):
+    newWord = ""
+    for char in word:
+        newWord += keys[char]
+
+    newWord = sortWord(newWord)
+    if newWord in patterns:
+        return patterns[newWord]
+    return None
+
+
+getPermutations([])
 ans = 0
 for line in lines:
-    keys = {}
-    rKeys = {}
     input, output = line.split(" | ")
-    words = [[] for _ in range(8)]
 
+    # Parse Input
+    words = []
     for word in input.split():
         length = len(word)
-        words[length].append(word)
+        sortedWord = sortWord(word)
+        words.append(sortedWord)
 
-    # print(line)
+    # Find Permutation
+    correctKeys = {}
+    for keys in permutations:
+        isValid = True
+        for word in words:
+            number = convert(word, keys)
+            if number is None:
+                isValid = False
+                break
+
+        if isValid:
+            correctKeys = keys
+            break
+
+    # Parse Output
+    number = 0
     for word in output.split():
-        newWord = ""
-        for char in word:
-            newWord += keys[char]
+        digit = convert(word, correctKeys)
+        number = number * 10 + digit
 
-        newWord = "".join(sorted(newWord))
-        number = number * 10 + patterns[newWord]
-
+    # Update Ans
     ans += number
+
 print(ans)
-
-
-# Junk
-
-# # Stage 1 - a
-# one = set(words[2][0])
-# seven = set(words[3][0])
-# diff = seven.difference(one)
-# diff = diff.pop()
-# keys[diff] = "a"
-# rKeys["a"] = diff
-
-# # Stage 2 - g
-# fourSeven = set.union(set(words[4][0]), set(words[3][0]))
-# nine = set()
-# strNine = ""
-# for word in words[6]:
-#     diff = set(word).difference(fourSeven)
-#     if len(diff) == 1:
-#         diff = diff.pop()
-#         keys[diff] = "g"
-#         rKeys["g"] = diff
-#         nine = set(word)
-#         strNine = word
-
-# # Stage 3 - d
-# exOne = set(words[2][0])
-# exOne.add(rKeys["a"])
-# exOne.add(rKeys["g"])
-# three = set()
-# for word in words[5]:
-#     diff = set(word).difference(exOne)
-#     if len(diff) == 1:
-#         diff = diff.pop()
-#         keys[diff] = "d"
-#         rKeys["d"] = diff
-#         three = set(word)
-
-# # Stage 4 - c
-# four = set(words[4][0])
-# for word in words[5]:
-#     diff = nine.difference(set(word))
-#     if len(diff) == 2:  # 2
-#         diff = one.difference(diff)
-#         diff = diff.pop()
-#         keys[diff] = "c"
-#         rKeys["c"] = diff
-
-# # Stage 5 - f
-# diff = three.difference(set(keys.keys()))
-# diff = diff.pop()
-# keys[diff] = "f"
-# rKeys["f"] = diff
-
-# # Stage 6 - b
-# diff = four.difference(set([rKeys["c"], rKeys["d"], rKeys["f"]]))
-# diff = diff.pop()
-# keys[diff] = "b"
-# rKeys["b"] = diff
-
-# # Stage 7 - e
-# eight = set(words[7][0])
-# diff = eight.difference(set(keys.keys()))
-# diff = diff.pop()
-# keys[diff] = "e"
-# rKeys["e"] = diff
-
-# number = 0
-# print(keys)
