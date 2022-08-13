f68df638aaf326661156061ab1429f5673a2c6e4
Alexandru Turcanu <pondorasti@gmail.com>
2022-08-12 20:34:03 -0700

    feat(LeetCode): Binary Search

---
+++ LeetCode/704 - Binary Search.ts
@@ -0,0 +1,18 @@
+function search(nums: number[], target: number): number {
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2)
+
+    if (nums[middle] === target) {
+      return middle
+    } else if (nums[middle] < target) {
+      left = middle + 1
+    } else {
+      right = middle - 1
+    }
+  }
+
+  return -1
+}
